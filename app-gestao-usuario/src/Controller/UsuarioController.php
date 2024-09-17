41a3625c1228c23eb5db7c351c0d831eb40eb516
Diego Bastos <d.bastos@marinha.mil.br>
2024-09-17 15:58:14 -0300

    Autenticação do microsserviço de gestão de usuário via bearer token no header

---
+++ app-gestao-usuario/src/Controller/UsuarioController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+class UsuarioController extends AbstractController
+{
+    #[Route('/usuario', name: 'app_usuario')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UsuarioController.php',
+        ]);
+    }
+}
--- app-gestao-usuario/src/Security/ApiKeyAuthenticator.php
+++ app-gestao-usuario/src/Security/ApiKeyAuthenticator.php
@@ -4,6 +4,10 @@
 namespace App\Security;
 
 use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +18,13 @@ use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Throwable;
 
 class ApiKeyAuthenticator extends AbstractAuthenticator
 {
+    public function __construct(private UsuarioRepository $usuarioRepository, private EntityManagerInterface $entityManager) {
+    }
+
     /**
      * Called on every request to decide if this authenticator should be
      * used for the request. Returning `false` will cause this authenticator
@@ -24,30 +32,51 @@ class ApiKeyAuthenticator extends AbstractAuthenticator
      */
     public function supports(Request $request): ?bool
     {
-        // "auth-token" is an example of a custom, non-standard HTTP header used in this application
-        return $request->headers->has('auth-token');
+        return $request->headers->has('Authorization');
     }
 
     public function authenticate(Request $request): Passport
     {
         $authorization = $request->headers->get('Authorization');
         if (null === $authorization) {
-            // The token header was empty, authentication fails with HTTP Status
-            // Code 401 "Unauthorized"
             throw new CustomUserMessageAuthenticationException('No API token provided');
         }
 
         // Extrai o token JWT do cabeçalho Authorization
         $jwt = str_replace('Bearer ', '', $authorization); // Remove "Bearer " do início
 
+        #// Lê a chave pública
+        $publicKeyPath = __DIR__ . '/public_key.pem';
+        $publicKey = openssl_pkey_get_public(file_get_contents($publicKeyPath));
 
-        // ### pendente validar o token e pegar o identificador. ### //
+        if (!$publicKey) {
+            throw new CustomUserMessageAuthenticationException('Erro ao carregar a chave pública! ' . openssl_error_string());
+        }
 
-        // implement your own logic to get the user identifier from `$apiToken`
-        // e.g. by looking up a user in the database using its API key
-        $userIdentifier = '';
+        try {
+            $decoded = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+            $arrayUsuario = (array) $decoded;
+        } catch (Throwable $ex) {
+            throw new CustomUserMessageAuthenticationException('JWT inválido! ' . $ex->getMessage());
+        }
+        
+        $usuario = $this->usuarioRepository->findOneBy(['uuid' => $arrayUsuario['sub']]);
 
-        return new SelfValidatingPassport(new UserBadge($userIdentifier));
+        //Cadastra o usuário caso não exista
+        if(!$usuario){
+            $usuario = new Usuario();
+            $usuario->setUuid($arrayUsuario['sub']);
+            $usuario->setNome($arrayUsuario['name']);
+            $usuario->setRoles(array('ROLE_USER'));
+
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $this->entityManager->persist($usuario);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $this->entityManager->flush();
+        }
+
+        return new SelfValidatingPassport(new UserBadge($usuario->getUuid()));
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
